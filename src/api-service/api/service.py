1bf1acc07a2b0506f269d7b6ed33743a901783de
shivasj <shivasj@gmail.com>
2025-11-15 10:43:28 -0500

    updated from v2

--- src/api-service/api/service.py
+++ src/api-service/api/service.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
 from api.routers import newsletter, podcast
+
 from api.routers import llm_chat, llm_cnn_chat
 from api.routers import llm_rag_chat, llm_agent_chat
-from api.routers import test_router
+
+# from api.routers import test_router
 
 # Setup FastAPI app
 app = FastAPI(title="API Server", description="API Server", version="v1")
@@ -23,6 +26,13 @@ app.add_middleware(
 async def get_index():
     return {"message": "Welcome to AC215"}
 
+
+@app.get("/square_root/")
+async def square_root(x: float = 1, y: float = 2):
+    z = x**2 + y**2
+    return z**0.5
+
+
 # Additional routers here
 app.include_router(newsletter.router, prefix="/newsletters")
 app.include_router(podcast.router, prefix="/podcasts")
@@ -30,4 +40,4 @@ app.include_router(llm_chat.router, prefix="/llm")
 app.include_router(llm_cnn_chat.router, prefix="/llm-cnn")
 app.include_router(llm_rag_chat.router, prefix="/llm-rag")
 app.include_router(llm_agent_chat.router, prefix="/llm-agent")
-app.include_router(test_router.router, prefix="/test")
+# app.include_router(test_router.router, prefix="/test")
