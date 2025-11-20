5af2ca2c373d981d3af60b2e6dacd8995ad66c69
shivasj <shivasj@gmail.com>
2025-11-20 07:43:53 -0500

    create network fixed

--- src/deployment/deploy_k8s/create_network.py
+++ src/deployment/deploy_k8s/create_network.py
@@ -2,20 +2,20 @@ import pulumi
 import pulumi_gcp as gcp
 
 
-def create_network(region):
+def create_network(region, app_name):
     # Create VPC network with manual subnet configuration for the cheese app
     network = gcp.compute.Network(
-        "cheese-app-vpc",
-        name="cheese-app-vpc",
+        f"{app_name}-vpc",
+        name=f"{app_name}-vpc",
         auto_create_subnetworks=False,
         routing_mode="REGIONAL",
-        description="VPC for Cheese App Kubernetes Cluster",
+        description="VPC for Kubernetes Cluster",
     )
 
     # Create subnet within the VPC with private Google access enabled
     subnet = gcp.compute.Subnetwork(
-        "cheese-app-subnet",
-        name="cheese-app-subnet",
+        f"{app_name}-subnet",
+        name=f"{app_name}-subnet",
         ip_cidr_range="10.0.0.0/19",
         region=region,
         network=network.id,
@@ -26,8 +26,8 @@ def create_network(region):
 
     # Create Cloud Router to enable NAT for private instances
     router = gcp.compute.Router(
-        "cheese-app-router",
-        name="cheese-app-router",
+        f"{app_name}-router",
+        name=f"{app_name}-router",
         network=network.id,
         region=region,
         opts=pulumi.ResourceOptions(depends_on=[network, subnet]),
@@ -35,8 +35,8 @@ def create_network(region):
 
     # Configure Cloud NAT to allow private instances to access the internet
     nat = gcp.compute.RouterNat(
-        "cheese-app-nat",
-        name="cheese-app-nat",
+        f"{app_name}-nat",
+        name=f"{app_name}-nat",
         router=router.name,
         region=region,
         nat_ip_allocate_option="AUTO_ONLY",
